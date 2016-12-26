28fe8155b34dda3b9d9cbe2234b35eb8aaaf10e5
Олег Арюков <oleg.aryukov@ediweb.ru>
2016-12-26 15:53:10 +0300

    refactor

---
+++ Chapter1/src/main/java/ru/aryukov/Calculate.java
@@ -0,0 +1,17 @@
+package ru.aryukov;
+/**
+ *Class Calculate решение задачи 1 урок 1.
+ *@author oaryukov
+ *@since 26.12.2016
+*/
+public class Calculate {
+/**
+ *main method.
+ *@author oaryukov
+ *@since 26.12.2016
+ *@param args - argument for main method
+*/
+	public static void main(String[] args) {
+		System.out.println("Hello world!");
+	}
+}
