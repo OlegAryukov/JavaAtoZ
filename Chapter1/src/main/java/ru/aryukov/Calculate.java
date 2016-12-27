6536a982bd4eb0daf81f4657b772d74c0220ce70
Олег Арюков <oleg.aryukov@ediweb.ru>
2016-12-27 12:02:44 +0300

    Calculator

--- Chapter1/src/main/java/ru/aryukov/Calculate.java
+++ Chapter1/src/main/java/ru/aryukov/Calculate.java
@@ -5,12 +5,53 @@ package ru.aryukov;
  *@since 26.12.2016
 */
 public class Calculate {
-/**
- *main method.
- *@since 26.12.2016
- *@param args - argument for main method
-*/
-	public static void main(String[] args) {
-		System.out.println("Hello world!");
+	/**
+	 * result of calculation.
+	 */
+	private double result;
+	/**
+	 * getter for result.
+	 * @return result
+	 */
+	public double getResult() {
+		return result;
+	}
+	/**
+	 *add method.
+	 *@since 26.12.2016
+	 *@param a - argument for add method
+	 *@param b - argument for add method
+	*/
+	public void add(double a, double b) {
+		result = a + b;
+	}
+	/**
+	 * substrauct method.
+	 * @param a - argument for substract
+	 * @param b - argument for substract
+	 */
+	public void substract(double a, double b) {
+		result = a - b;
+	}
+	/**
+	 * div method.
+	 * @param  b - argument for substract
+	 * @param a -argument for substract
+	 */
+	public void div(double a, double b) {
+		try {
+			result = a / b;
+		} catch (ArithmeticException e) {
+			e.printStackTrace();
+		}
+	}
+	/**
+	 * multiplay method.
+	 * @param a - param for method
+	 * @param b - argument for substract
+	 */
+	public  void multiplay(double a, double b) {
+		result = a * b;
 	}
 }
+
--- Chapter1/src/test/java/ru/aryukov/CalculateTest.java
+++ Chapter1/src/test/java/ru/aryukov/CalculateTest.java
@@ -2,21 +2,41 @@ package ru.aryukov;
 
 import org.junit.*;
 
-import java.io.ByteArrayOutputStream;
-import java.io.PrintStream;
-
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
 public class CalculateTest {
+    Calculate calculate = new Calculate();
     /**
      * Test add
      */
     @Test
-    public void whenAddOneToOneThwmTwo() {
-        ByteArrayOutputStream out = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(out));
-        Calculate.main(null);
-        assertThat(out.toString(),is(String.format("Hello world!\n",System.getProperty("line.separator"))));
+    public void whenAdd() {
+        calculate.add(2.5, 3.5);
+        assertThat(calculate.getResult(),is(6.0));
+    }
+    /**
+     * Test substract
+     */
+    @Test
+    public void whenSub() {
+        calculate.substract(2.6,2.1);
+        assertThat(calculate.getResult(), is(0.5));
+    }
+    /**
+     * Test div
+     */
+    @Test
+    public void whenDiv() {
+        calculate.div(9.0,3.0);
+        assertThat(calculate.getResult(), is(3.0));
+    }
+    /**
+     * Test multiplay
+     */
+    @Test
+    public void whenMultiply() {
+        calculate.multiplay(3.0,3.0);
+        assertThat(calculate.getResult(), is(9.0));
     }
 }
