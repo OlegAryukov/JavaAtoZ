45458c4bee32d141d05f08bbcafe687c16a6bf2b
oaryukov <olegaryukov@gmail.com>
2017-01-05 15:16:55 +0300

    commit TriTest

--- Chapter1/src/test/java/ru/aryukov/triangle/TriangleTest.java
+++ Chapter1/src/test/java/ru/aryukov/triangle/TriangleTest.java
@@ -2,16 +2,19 @@ package ru.aryukov.triangle;
 
 import org.junit.Test;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
 /**
  * Created by olega on 27.12.16.
  */
 public class TriangleTest {
     Point a = new Point(2,2);
-    Point b = new Point(2, -2);
+    Point b = new Point(2,-2);
     Point c = new Point(0,0);
-    Point d = new Point()
+    //Point d = new Point()
     @Test
     public void calculatingDistance(){
-
+        assertThat(a.distanceTo(b),is(4.0));
     }
 }
