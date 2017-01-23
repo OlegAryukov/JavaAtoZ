32a5db23a6cdddb3248d65ff922607c8d6b39c0b
oaryukov <olegaryukov@gmail.com>
2017-01-23 23:51:50 +0300

    SimpleArray

---
+++ Chapter5/src/main/java/ru/aryukov/SimpleArray/SimpleArray.java
@@ -0,0 +1,23 @@
+package ru.aryukov.SimpleArray;
+
+/**
+ * Created by oaryukov on 23.01.2017.
+ */
+public class SimpleArray<E> {
+
+    private Object[] conteiner;
+    private int index = 0;
+
+    public SimpleArray(int size) {
+        this.conteiner = new Object[size];
+    }
+
+    public void add(E element){
+        conteiner[index++] = element;
+    }
+
+    public E get(int position){
+        return (E) conteiner[position];
+    }
+
+}
